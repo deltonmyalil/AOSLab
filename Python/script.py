feda0436b2954718ef9b8e38e0f5e3f7e14f656c
Delton Myalil Antony <36544374+deltonmyalil@users.noreply.github.com>
2018-08-06 16:59:26 +0530

    Add files via upload

--- Python/script.py
+++ Python/script.py
@@ -1,6 +1,12 @@
 import subprocess as sp
 
-myFile = open("output.txt", "a")
+myFile = open("output.txt", "w")
 sp.Popen("ls", stdout = myFile)
 sp.Popen(["ls","-l"], stdout = myFile)
 sp.call(["mkdir" , "delton"])
+sp.Popen("ls", stdout = myFile)
+## added another folder and called ls
+sp.Popen(["ls","-a"], stdout = myFile)
+sp.Popen(["ls","-s"], stdout = myFile)
+sp.Popen(["ls","-R"], stdout = myFile)
+sp.Popen(["ls","-S"], stdout = myFile)
